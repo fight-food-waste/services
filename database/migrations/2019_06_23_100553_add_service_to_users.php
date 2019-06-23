453e9ffbf59327b20215f538021cc876e18a6f65
angristan <angristan@pm.me>
2019-06-23 12:33:06 +0200

    Add reverse migration for AddServiceToUsers

--- database/migrations/2019_06_23_100553_add_service_to_users.php
+++ database/migrations/2019_06_23_100553_add_service_to_users.php
@@ -27,7 +27,7 @@ class AddServiceToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('service_id');
         });
     }
 }
