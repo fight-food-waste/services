f0e72d1a3c8a3b4e16704055d5a5591378cadb2b
angristan <angristan@pm.me>
2019-07-11 23:36:52 +0200

    Test mailhog

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
+use Mail;
 
 class HomeController extends Controller
 {
@@ -25,6 +26,15 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        Mail::send('welcome', [], function ($message)
+        {
+
+            $message->from('me@gmail.com', 'Christian Nwamba');
+
+            $message->to('chrisn@scotch.io');
+
+        });
+
         return view('home', [
             'user' => $request->user()
         ]);
